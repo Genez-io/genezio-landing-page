89a7593bf3a036a214da0b7b6beb5af2eb31b69c
Bogdan Vlad <vladbgd@gmail.com>
2025-07-02 13:16:44 +0300

    Debug

--- new-landing/src/app/(components)/header.tsx
+++ new-landing/src/app/(components)/header.tsx
@@ -102,7 +102,7 @@ export function Header() {
               onClick={() => (window.location.href = "/blog/")}
               className="text-slate-700 dark:text-slate-300 hover:text-purple-600 dark:hover:text-purple-400"
             >
-              Blog
+              Blog Posts
             </button>
 
             <button
